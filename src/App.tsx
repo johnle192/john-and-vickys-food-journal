2032048f3107d25fd4bd4f32ad0047164aa8a251
John Le <john.le192@gmail.com>
2024-05-11 11:15:10 -0700

    add Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import RestaurantList from './components/RestaurantList.tsx';
+import Map from './components/Map.tsx';
 
 function App() {
   return (
     <div className="flex min-h-full w-screen flex-col">
-      <header className="shrink-0 border-b border-gray-200 bg-white">
+      <header className="shrink-0 border-b border-gray-200 bg-white fixed top-0 left-0 right-0 z-50">
         <div className="mx-auto flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
           <h3 className="text-base font-semibold leading-6 text-gray-900">
             {/* eslint-disable-next-line react/no-unescaped-entities */}
@@ -13,10 +14,14 @@ function App() {
         </div>
       </header>
 
-      <div className="mx-auto flex w-full items-start gap-x-8 px-4 py-10 sm:px-6 lg:px-8">
+      <div className="mx-auto flex w-full items-start gap-x-8 px-4 py-24 sm:px-6 lg:px-8">
         <RestaurantList />
 
-        <main className="flex-1">{/* Main area */}</main>
+        <main className="flex-1 h-svh w-3/5 relative">
+          <div className="fixed h-full w-full">
+            <Map />
+          </div>
+        </main>
       </div>
     </div>
   );
---
+++ src/components/Map.tsx
@@ -0,0 +1,39 @@
+import mapboxgl from 'mapbox-gl';
+import { useEffect, useRef } from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+export default function Map() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = String(import.meta.env.VITE_MAPBOX_TOKEN);
+
+    if (mapContainer.current) {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-122.350616, 47.6206],
+        zoom: 10
+      });
+
+      map.on('wheel', (event) => {
+        if (
+          event.originalEvent.ctrlKey ||
+          event.originalEvent.metaKey ||
+          event.originalEvent.altKey
+        ) {
+          return;
+        }
+
+        event.preventDefault();
+      });
+      map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+      return () => map.remove();
+    }
+  }, []);
+  return (
+    <div ref={mapContainer} className="h-full w-svw">
+      MAPPPPPPP
+    </div>
+  );
+}
